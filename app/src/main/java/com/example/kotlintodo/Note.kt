ff2d7f5b564197741184bdf85e02494d96539be4
Ludvins <theludvins@gmail.com>
2019-08-29 21:08:12 +0200

    Made Note class Parcelable

--- app/src/main/java/com/example/kotlintodo/Note.kt
+++ app/src/main/java/com/example/kotlintodo/Note.kt
@@ -1,31 +1,24 @@
 package com.example.kotlintodo
 
-class Note {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Note(
+    var id : Int,
+    var title: String,
+    var content : String
+) : Parcelable{
 
     companion object {
         // Similar to a static var, used to set different IDs to each note.
         var count = 0
     }
 
+    constructor(title: String, content: String) : this(++count, title, content)
 
-    var id: Int? = null
-    var title: String? = null
-    var content: String? = null
-
-    constructor(title: String, content: String) {
-        // Every new note must be created using this.
-        this.id = ++count
-        this.title = title
-        this.content = content
-    }
-
-    constructor(id: Int, title: String, content: String){
-        // Every note created from de database must be created using this.
-        this.id = id
-        this.title = title
-        this.content = content
-
-        if (id > count)
+    init {
+        if (this.id > count)
             // When the program ends, count sets to 0, this is a way to recover its value when reading notes from the database at startup.
             count = id
     }
